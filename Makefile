03be0ac3b0600594ea2fa1d3979c78c316e13dd2
Ceasar Bautista <cbautista2010@gmail.com>
2014-09-17 10:00:41 -0700

    Configure test pusher event

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 .PHONY: server
 
-db.sqlite3:
-	python manage.py migrate
+env: requirements.txt
+	virtualenv env
+	pip install -r requirements.txt
 
-server: db.sqlite3
-	python manage.py runserver
+db.sqlite3: env
+	source env/bin/activate & python manage.py migrate
+
+server: db.sqlite3 env
+	source env/bin/activate & python manage.py runserver
--- chatroom/views.py
+++ chatroom/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+import pusher
 
 
 def index(request):
     template = loader.get_template('chatroom/index.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
+
+
+def message(request):
+    p = pusher.Pusher(
+          app_id='89800',
+          key='ceb9e10775a1d1de32b6',
+          secret='9be17a0bb5d5b5079d79'
+    )
+    p['test_channel'].trigger('test_event', {'message': 'hello world'})
