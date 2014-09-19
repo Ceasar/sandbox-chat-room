1a212affab3db7c9f314ff3ac65ed373ef02e666
Ceasar Bautista <cbautista2010@gmail.com>
2014-09-19 15:41:42 -0700

    Send messages in the background
    This fixes a potential scaling problems, where if triggering the Pusher
    event had a high latency, responses to the client might stall while waiting
    for Pusher, wasting available threads and possibly causing requests to
    timeout (as no thread is assigned to them, despite a connection).

--- chatroom/views.py
+++ chatroom/views.py
@@ -1,3 +1,5 @@
+import threading
+import Queue
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.views.decorators.http import require_POST
@@ -7,22 +9,33 @@ import pusher
 
 from chatroom.settings import PUSHER_SECRET
 
+q = Queue.Queue()
+
 
 def index(request):
     context = RequestContext(request, {})
     return render_to_response('chatroom/index.html', context)
 
 
-@require_POST
-def message(request):
+def worker():
     p = pusher.Pusher(
         app_id='89800',
         key='ceb9e10775a1d1de32b6',
         secret=PUSHER_SECRET,
     )
+    while True:
+        data = q.get()
+        p['test_channel'].trigger('test_event', data)
+
+thread = threading.Thread(target=worker)
+thread.start()
+
+
+@require_POST
+def message(request):
     data = {
         'name': request.POST['name'],
         'message': request.POST['message'],
     }
-    p['test_channel'].trigger('test_event', data)
+    q.put(data)
     return HttpResponse(status=204)
